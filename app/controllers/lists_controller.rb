41610086e2661d223fcb0dc86e3531a19f1fa90f
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-07 10:57:13 +0000

    [Finish] 8章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,12 +7,15 @@ class ListsController < ApplicationController
   # 以下を追加した
   def create
     # 1 .&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    if @list.save
     # redirect_to '/top'を削除して、以下コードに変更
     # 4. 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -24,11 +27,24 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を1件取得
+    list.destroy #データを削除
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
